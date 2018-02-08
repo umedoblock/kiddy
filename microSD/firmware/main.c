05ba9c987cdc43138ef17862d62d368786922904
umedoblock <umedoblock@gmail.com>
2018-02-08 23:01:05 +0900

    atmega328p に書き込めば，microSDに書き込めるfirmware。

--- microSD/firmware/main.c
+++ microSD/firmware/main.c
@@ -8,26 +8,33 @@
 FATFS FatFs;		/* FatFs work area needed for each volume */
 FIL Fil;			/* File object needed for each open file */
 
+int init_device(void)
+{
+	return 0;
+}
 
 int main (void)
 {
 	UINT bw;
+	FRESULT res;
+	BYTE mode;
 
+	init_device();
 
-	f_mount(&FatFs, "", 0);		/* Give a work area to the default drive */
+	res=f_mount(&FatFs, "", 0);		/* Give a work area to the default drive */
 
-	if (f_open(&Fil, "newfile.txt", FA_WRITE | FA_CREATE_ALWAYS) == FR_OK) {	/* Create a file */
+	if (res != FR_OK) {
+		// light_error_onoff(res);
+		return res;
+	}
 
-		f_write(&Fil, "It works!\r\n", 11, &bw);	/* Write data to the file */
+	mode=FA_WRITE | FA_CREATE_ALWAYS;
+	res=f_open(&Fil, "newfile.txt", mode);
 
-		f_close(&Fil);								/* Close the file */
-
-		if (bw == 11) {		/* Lights green LED if data written well */
-			DDRB |= 0x10; PORTB |= 0x10;	/* Set PB4 high */
-		}
+	if (res == FR_OK) {	/* Create a file */
+		res=f_write(&Fil, "It works!\r\n", 11, &bw);	/* Write data to the file */
+		res=f_close(&Fil);								/* Close the file */
 	}
 
 	for (;;) ;
 }
-
-
